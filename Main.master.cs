eab27a737c3a7e1e32ea133eabfb5e549663ebfe
Yornie Westink <jinxie88@live.nl>
2018-06-05 11:38:25 +0200

    Adding connection string

--- Main.master.cs
+++ Main.master.cs
@@ -5,15 +5,16 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.OleDb;
+using System.Configuration;
 
 public partial class Main : System.Web.UI.MasterPage
 {
     private OleDbConnection _conn;
-    
 
     protected void Page_Load(object sender, EventArgs e)
     {
-       /* _conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Server.MapPath(@"\App_data") + @"\JECODatabase.accdb";
+        _conn = new OleDbConnection();
+        _conn.ConnectionString = ConfigurationManager.ConnectionStrings["JECO"].ToString();
 
          OleDbCommand cmd = new OleDbCommand();
          cmd.Connection = _conn;
@@ -32,7 +33,7 @@ public partial class Main : System.Web.UI.MasterPage
          finally
          {
              _conn.Close();
-         }*/
+         }
 
     }
 
