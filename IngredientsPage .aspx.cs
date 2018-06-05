37599ed4015ec148beee16d706eeba41b7e684db
Yornie Westink <jinxie88@live.nl>
2018-06-05 14:45:42 +0200

    Revert "Revert "DB connection""
    This reverts commit f29db641605dd1d9605168ca4bf435d6caa6cd4e.

--- IngredientsPage .aspx.cs
+++ IngredientsPage .aspx.cs
@@ -8,11 +8,27 @@ using System.Data.OleDb;
 
 public partial class IngredientsPage : System.Web.UI.Page
 {
-   
+    private OleDbConnection _conn;
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+        if(Main.Conn() != null)
+        {
+            _conn = Main.Conn();
+            _conn.Open();
+
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = _conn;
+            cmd.CommandText = "SELECT * FROM product";
+
+            OleDbDataReader r = cmd.ExecuteReader();
+            r.Read();
+            while (r.Read())
+            {
+                IngredientTestLabel.Text = String.Format("{0}: {1}<br /> \n", r["ProductNr"].ToString(), r["Naam"].ToString());
+            }
+
+        }
 
     }
 }
